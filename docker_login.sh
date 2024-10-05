3b1023289532c68ebae87975f6e076cecf85d18e
Rewatiraman Sahu <67386448+rewatiramansahu@users.noreply.github.com>
2024-10-05 10:53:47 +0530

    Update docker_login.sh

--- docker_login.sh
+++ docker_login.sh
@@ -2,7 +2,13 @@
 
 # Set your Docker Hub credentials
 DOCKER_USERNAME='rewatiramansahu'   # Username passed as the first argument
-DOCKER_PASSWORD='Test@12345'   # Password passed as the second argument
+# DOCKER_PASSWORD='Test@12345'   # Password passed as the second argument
+SECRET_KEY="sahu" 
+ENCRYPTED_PASSWORD="U2FsdGVkX19mGSzJrQfOFL9lNtjuoJ3iOIaPeP/CZCk="
+
+# Decrypt the Docker Hub password
+
+DOCKER_PASSWORD=$(echo "$ENCRYPTED_PASSWORD" | openssl enc -aes-256-cbc -d -a -pass pass:"$SECRET_KEY")
 
 # Log in to Docker Hub
 echo "Logging in to Docker Hub..."
